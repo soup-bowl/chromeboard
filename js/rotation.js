1fc5dab7f7be7c2719766b662b21e0632832d02b
soup-bowl <soup-bowl@users.noreply.github.com>
2016-11-23 23:41:14 +0000

    Basic functionality improvements.
    Site can now loop through the provided pages in storage.

--- js/rotation.js
+++ js/rotation.js
@@ -1,11 +1,37 @@
 $(document).ready(function() {
+    var opt;
     chrome.storage.sync.get('chromeboardPrefs', function (obj) {
-        var opt = obj.chromeboardPrefs.urlCol;
-        //document.getElementById("sites").value = ArrayToSite( opt );
+        opt = obj.chromeboardPrefs.urlCol;
         console.log('chromeboardPrefs', opt);
 
         for (var i = 0; i < opt.length; i++) {
-            $("#site-collection").append("<iframe src=\"" + opt[i] + "\"></iframe>")
+            $("#site-collection").append("<iframe id=\"site-" + (i + 1) + "\" src=\"" + opt[i] + "\"></iframe>")
         }
+
+        slide(10, opt.length, -1);
     });
+      
 });
+
+function slide(repeats, count, current) {
+    nextActive = current + 1;
+    if (nextActive > (count -1)) {
+        nextActive = 0;
+    }
+
+    if (repeats > 0) {
+        for (var i = 0; i < count; i++) {    	
+            $('#site-'+(i + 1)).removeClass('active');
+            if (nextActive == i) {
+                $('#site-'+(i + 1)).addClass('active');
+            }
+        }
+        
+        window.setTimeout(
+          function(){
+            slide(repeats, count, nextActive)
+          },
+          30000
+        );
+    }
+}
