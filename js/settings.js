002770d614fa367d810526a32c13767626053efa
soup-bowl <soup-bowl@users.noreply.github.com>
2016-11-23 06:50:55 +0000

    Internal checkpoint.

--- js/settings.js
+++ js/settings.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-    purgeUserPrefs();
-    storeUserPrefs(["example.com","dave.com","woohoo.com"]);
+    document.getElementById("saveS").addEventListener("click",goSettings);
+    document.getElementById("sites").value
+    //purgeUserPrefs();
+    //storeUserPrefs(["example.com","dave.com","woohoo.com"]);
     getUserPrefs();
 });
 
 var storage = chrome.storage.sync;
 
+function goSettings() {
+    console.log(
+        siteToArray(document.getElementById("sites").value)
+    );
+    storeUserPrefs( siteToArray(document.getElementById("sites").value) );
+}
+
 function storeUserPrefs(urlCollection) {
+    storage.clear();
     var key='chromeboardPrefs', testPrefs = {'urlCol': urlCollection};
         storage.set({"chromeboardPrefs": testPrefs}, function() {
             //console.log('Saved', key, testPrefs);
@@ -14,11 +24,19 @@ function storeUserPrefs(urlCollection) {
 }
 
 function getUserPrefs() {
-    storage.get('chromeboardPrefs', function (obj) {
+    var aaa = storage.get('chromeboardPrefs', function (obj) {
         //console.log('chromeboardPrefs', obj);
-    });
+        return obj.chromeboardPrefs.urlCol;
+    });  
+
+    console.log(aaa);
 }
 
 function purgeUserPrefs() {
-    chrome.storage.sync.clear();
+    storage.clear();
+}
+
+function siteToArray(textareaContent) {
+    comSepStr = textareaContent.replace(/(\r\n|\n|\r)/gm,",");
+    return comSepStr.split(",");
 }
