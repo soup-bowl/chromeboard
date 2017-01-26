5a7d1cb8116513d652e1a8ff779db4f1bd942bce
soup-bowl <soup-bowl@users.noreply.github.com>
2017-01-26 23:08:43 +0000

    Partial rotation reworking.
    Reworking rotation to separate the rotation functionality from the loop. Partial space to tab movement.

--- js/rotation.js
+++ js/rotation.js
@@ -1,46 +1,52 @@
-$(document).ready(function() {
-    var opt;
-    chrome.storage.sync.get('chromeboardPrefs', function (obj) {
-        opt = obj.chromeboardPrefs.urlCol;
-        
-        if (opt.length > 0 && opt != "") {
-            for (var i = 0; i < opt.length; i++) {
-                $("#site-collection").append("<iframe id=\"site-" + (i + 1) + "\" src=\"" + opt[i] + "\"></iframe>");
-            }
-        } else {
-            $("#site-collection").append("<iframe id=\"site-1\" src=\"blankingpage.html\" class=\"active\"></iframe>");
-        }
+// Global rotation variables.
+var ROTATION_CURRENT = -1;
+var ROTATION_TOTAL   = 0;
+var ROTATION_PAUSED  = false;
 
-        slide(10, opt.length, -1, obj.chromeboardPrefs.transitionTime);
-    });
-      
+$(document).ready(function() {
+	var opt;
+	chrome.storage.sync.get('chromeboardPrefs', function (obj) {
+		opt = obj.chromeboardPrefs.urlCol;
+		ROTATION_TOTAL = opt.length;
+		
+		if (opt.length > 0 && opt != "") {
+			for (var i = 0; i < opt.length; i++) {
+				$("#site-collection").append("<iframe id=\"site-" + (i + 1) + "\" src=\"" + opt[i] + "\"></iframe>");
+			}
+		} else {
+			$("#site-collection").append("<iframe id=\"site-1\" src=\"blankingpage.html\" class=\"active\"></iframe>");
+		}
+
+		slide(10, obj.chromeboardPrefs.transitionTime);
+	});
+	  
 });
 
 // -- Settings button and background handlers --
 $("#settclick").click(function(event) {
-    toggleSettingsFrame(event);
+	toggleSettingsFrame(event);
 });
 
 $(".settings-modal-close-shadow").click(function(event) {
-    toggleSettingsFrame(event);
+	toggleSettingsFrame(event);
 });
 // -- End of settings button and background handlers --
 
 // -- Lockstate handlers --
 $("#setlockstate").click(function(event) {
-    toggleLockstate(event);
+	toggleLockstate(event);
 });
 
 $("#setunlockstate").click(function(event) {
-    toggleLockstate(event);
+	toggleLockstate(event);
 });
 
 $(document).on({
-    "contextmenu": function(e) {
-        if($('#sb-unlocked').hasClass('hidden')) {
-            e.preventDefault();
-        }
-    }
+	"contextmenu": function(e) {
+		if($('#sb-unlocked').hasClass('hidden')) {
+			e.preventDefault();
+		}
+	}
 });
 
 // --  End of lockstate handlers --
@@ -50,15 +56,15 @@ $(document).on({
  * @param {event} event
  */
 function toggleSettingsFrame(event) {
-    event.preventDefault();
-    if( $('.settings-modal').hasClass('active') ) {
-        $('.settings-modal-close-shadow').removeClass('active');
-        $('.settings-modal').removeClass('active');
-        window.location.reload();
-    } else {
-        $('.settings-modal-close-shadow').addClass('active');
-        $('.settings-modal').addClass('active');
-    }
+	event.preventDefault();
+	if( $('.settings-modal').hasClass('active') ) {
+		$('.settings-modal-close-shadow').removeClass('active');
+		$('.settings-modal').removeClass('active');
+		window.location.reload();
+	} else {
+		$('.settings-modal-close-shadow').addClass('active');
+		$('.settings-modal').addClass('active');
+	}
 }
 
 /**
@@ -66,58 +72,74 @@ function toggleSettingsFrame(event) {
  * @param {event} event
  */
 function toggleLockstate(event) {
-    event.preventDefault();
-    if( $('#sb-unlocked').hasClass('hidden') ) {
-        $('#sb-unlocked').removeClass('hidden');
-        $('#sb-locked').addClass('hidden');
+	event.preventDefault();
+	if( $('#sb-unlocked').hasClass('hidden') ) {
+		$('#sb-unlocked').removeClass('hidden');
+		$('#sb-locked').addClass('hidden');
 
-        $('.noclick-zone').remove();
-    } else {
-        $('#sb-unlocked').addClass('hidden');
-        $('#sb-locked').removeClass('hidden');
-        
-        $('body').append( $('<div/>').addClass("noclick-zone") );
-    }
+		$('.noclick-zone').remove();
+	} else {
+		$('#sb-unlocked').addClass('hidden');
+		$('#sb-locked').removeClass('hidden');
+		
+		$('body').append( $('<div/>').addClass("noclick-zone") );
+	}
 }
 
 /**
  * Infinite loop handling the tab switching process.
- *  
- * Repeats - Defunct, amount of times to repeat. Input less than 0 to stop entirely.
- * 
- * Count - Number of sites in the rotation.
- * 
- * Current - The site currently designated to be shown.
- * 
- * Duration - Length of time on the tab.
- * @param {integer} repeats
- * @param {integer} count 
- * @param {integer} current 
- * @param {integer} duration 
+ * @param {integer} repeats Defunct, amount of times to repeat. Input less than 0 to stop entirely.
+ * @param {integer} duration Length of time on the tab.
  */
-function slide(repeats, count, current, duration) {
-    nextActive = current + 1;
-    if (nextActive > (count -1)) {
-        nextActive = 0;
-    }
+function slide(repeats, duration) {
+	if (duration < 1) {
+		duration = 1;
+	}
 
-    if (duration < 1) {
-        duration = 1;
-    }
+	if (!ROTATION_PAUSED) {
+		rotation();
+	}	
+	
+	window.setTimeout(
+		function(){
+		slide(repeats, duration)
+		},
+		(duration * 1000)
+	);
+}
 
-    if (repeats > 0) {
-        for (var i = 0; i < count; i++) {    	
-            $('#site-'+(i + 1)).removeClass('active');
-            if (nextActive == i) {
-                $('#site-'+(i + 1)).addClass('active');
-            }
-        }
-        
-        window.setTimeout(
-          function(){
-            slide(repeats, count, nextActive, duration)
-          },
-          (duration * 1000)
-        );
-    }
+/**
+ * Switches the current tab. Allows specifying of the desired page number (listing order).
+ * 
+ * @param {integer} newtab If unchanged, goes to next in line.
+ */
+function rotation(newtab = false) {
+	nextActive = ROTATION_CURRENT + 1;
+	if (nextActive > (ROTATION_TOTAL - 1)) {
+		nextActive = 0;
+	}
+	
+	// New tab wasn't specified.
+	if (newtab === false) {
+		for (var i = 0; i < ROTATION_TOTAL; i++) {    
+			// Each site to the count of the rotation, remove (if any) the 'active' class, 
+			// and add it to the selected one.	
+			$('#site-'+(i + 1)).removeClass('active');
+			
+			if (nextActive == i) {
+				$('#site-'+(i + 1)).addClass('active');
+				ROTATION_CURRENT = nextActive;
+			}
+		}
+	} else {
+		for (var i = 0; i < ROTATION_TOTAL; i++) {    
+			// TODO - Remove code duplication.
+			$('#site-'+(i + 1)).removeClass('active');
+			
+			if (i == newtab) {
+				$('#site-'+(i + 1)).addClass('active');
+				ROTATION_CURRENT = newtab;
+			}
+		}
+	}
 }
